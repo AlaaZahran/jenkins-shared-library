aa19f811611e90e14f26170add51a531fc8a9814
alaaZahran <alaazahran4@gmail.com>
2023-04-13 04:03:39 +0200

    seprate function

--- src/com/example/Docker.groovy
+++ src/com/example/Docker.groovy
@@ -1,16 +1,22 @@
 #!/usr/bin/env groovy
 package com.example
-class Docker implements serializable{
+class Docker implements Serializable{
     def script
     Docker(script){
     this.script=script
      }
-    def buildDockerImage(string imageName){
+    def buildDockerImage(String imageName){
         script.echo ' building docker image .....'
+        script.sh "docker build . -t $imageName "
+
+    }
+    def dockerLogin(){
         script.withCredentials([script.usernamePassword(credentialsId: 'dockerhub', usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD')]) {
-            script.sh "docker build . -t $imageName "
             script.sh "echo $script.PASSWORD | docker login -u $script.USERNAME --password-stdin"
-            script.echo "push docker image to dockerhub"
-            script.sh "docker push $imageName"}
+            }
+    }
+    def pushDockerImage(String imageName){
+        script.echo "push docker image to dockerhub"
+        script.sh "docker push $imageName"
     }
 }
--- vars/buildDockerImage.groovy
+++ vars/buildDockerImage.groovy
@@ -1,5 +1,5 @@
 #!/usr/bin/env groovy
 import com.example.Docker
-def call(string imageName){
+def call(String imageName){
     return new Docker(this).buildDockerImage(imageName)
 }
